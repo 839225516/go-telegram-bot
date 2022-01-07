9aa8d96e6da68caea120fd63657eb7f3d51749d5
zengxianglin <839225516@qq.com>
2022-01-07 16:34:41 +0800

    add github action

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"flag"
 	"go-telegram-bot/app"
+	"k8s.io/klog/v2"
 )
 
 func main()  {
 
+	klog.InitFlags(nil)
+
 	// parse klog/v2 flags
 	flag.Parse()
+
+	// make sure we flush before exiting
+	defer klog.Flush()
 
 	app.Start()
 }
